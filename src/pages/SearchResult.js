0265cbdecb1fda561bcb365903ae098ef43b2a03
Hengameh Nami <hnami00@mylangara.ca>
2026-01-29 15:36:35 -0800

    serach page UI

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -1,14 +1,96 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { Component } from 'react'
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import React, { useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AppTextInput from "../components/AppTextInput";
+import Dropdown from "../components/Dropdown";
+import { colors } from "../utils/colors";
 
-export default class SearchResult extends Component {
-  render() {
-    return (
-      <View>
-        <Text>SearchResult</Text>
-      </View>
-    )
-  }
+function RequiredLabel({ children }) {
+  return (
+    <View style={styles.labelRow}>
+      <Text style={styles.label}>{children}</Text>
+      <Text style={styles.required}>*</Text>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+export default function SearchResult() {
+  const [name, setName] = useState("");
+  const [searchType, setSearchType] = useState("multi");
+
+  return (
+    <View style={styles.container}>
+      <RequiredLabel>Search Movie/TV Show Name</RequiredLabel>
+
+      <AppTextInput
+        icon="magnify"
+        placeholder="ie, James Bond, CSI"
+        autoCorrect={false}
+        spellCheck={false}
+        onChangeText={(t) => {
+          setName(t);
+        }}
+      />
+      <RequiredLabel>Choose Search Type</RequiredLabel>
+      <View style={styles.search}>
+        <View>
+          <Dropdown
+            selected={searchType}
+            list={["movie", "multi"]}
+            onSelect={setSearchType}
+          />
+          <Text style={{ fontSize: 9 }}>Please Select a Search Type</Text>
+        </View>
+        <Pressable style={styles.btn}>
+          <MaterialCommunityIcons
+            name="magnify"
+            size={20}
+            color={colors.white}
+            style={styles.icon}
+          />
+          <Text style={styles.btnText}>Search</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: colors.buttonBlue,
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 5,
+    width: "30%",
+    height: 40,
+    justifyContent: "center",
+  },
+  btnText: {
+    fontSize: 13,
+    color: colors.white,
+    textAlign: "center",
+    fontWeight: "600",
+  },
+  container: {
+    flex: 1,
+    padding: 40,
+    gap: 5,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+
+  labelRow: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  required: { color: "red", marginLeft: 2, fontSize: 14 },
+  search: {
+    flexDirection: "row",
+    gap: 10,
+  },
+});
