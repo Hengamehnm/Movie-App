beb4588847d2ee3754657fa129d4f7e58576f511
Hengameh Nami <hnami00@mylangara.ca>
2026-01-31 16:12:09 -0800

    .env fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,11 @@
-import { TMDB_API_KEY, TMDB_ACCESS_TOKEN } from "./secret";
+import Constants from "expo-constants";
+
+const extra =
+  Constants.expoConfig?.extra ??
+  Constants.manifest?.extra ??
+  Constants.manifest2?.extra ??
+  {};
 
 export const URL = "https://api.themoviedb.org/3";
-export const API_KEY = TMDB_API_KEY;
-export const ACCESS_TOKEN = TMDB_ACCESS_TOKEN;
+export const API_KEY = extra.TMDB_API_KEY;
+export const ACCESS_TOKEN = extra.TMDB_ACCESS_TOKEN;
